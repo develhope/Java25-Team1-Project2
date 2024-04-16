5bf3735c2c8c9c45a47be289f574108ba2b8e01b
Ilaria <ilaria.faleschini@gmail.com>
2024-04-16 16:23:25 +0200

    Prodotto Tablet

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         Nootebook notebook1 = new Nootebook(TipoDispositivo.notebook, 1, "Apple", "Macbook Pro", 17.5, 256, 1100.99, 1350.99);
+        Tablet tablet1 = new Tablet(TipoDispositivo.tablet, 1, "Samsung", "Galaxy Tab S9 Series", 14.6, 160, 1100.99, 1299.99);
 
         System.out.println(notebook1);
         System.out.println("Descrizione: " + notebook1.leggiDescrizioneProdotto());
+
+        System.out.println(tablet1);
+        System.out.println("Modello: " + tablet1.leggiModello());
     }
 
 }
