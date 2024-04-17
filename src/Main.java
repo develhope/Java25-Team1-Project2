b1ba37e8a355518b5aeb2281da8fa54706aa39ef
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2024-04-17 11:46:15 +0200

    implementazxione classse Smartphone

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Smartphone smartphone1 = new Smartphone(TipoDispositivo.smartphone, 21, "Asus", "Rog Phone 7 Ultimate", 6.78,512, 1499.00,1599.00);
+        System.out.println(smartphone1);
+        System.out.println("Modello: " + smartphone1.leggiModello());
+        System.out.println("Descrizione: " + smartphone1.leggiDescrizioneProdotto());
     }
 
 }
