f06f21c9967fd0283ce11c26d3fedd0ad78b0c11
Ilaria Faleschini <ilaria.faleschini@gmail.com>
2024-05-10 16:22:47 +0200

    Merge dal branch develop

--- src/Metodi.java
+++ src/Metodi.java
@@ -4,7 +4,7 @@ public class Metodi {
 
     // Metodo per stampare tutti i dispositivi presenti nel magazzino
     public ArrayList<Prodotti> stampaDispositiviMagazzino(Magazzino magazzino) {
-        ArrayList<Prodotti> dispositivi = magazzino.getProdottiInMagazzino();
+        ArrayList<Prodotti> dispositivi = (ArrayList<Prodotti>) magazzino.getProdottiInMagazzino();
 
         if (dispositivi.isEmpty()) {
             System.out.println("Nessun dispositivo presente in magazzino.");
