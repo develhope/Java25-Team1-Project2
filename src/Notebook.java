fa1da201e7537a23cd039ccc6ed6bc75efbae632
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2024-05-11 11:18:56 +0200

    conversione tipi primitivi in oggetti

--- src/Notebook.java
+++ src/Notebook.java
@@ -2,8 +2,9 @@ import java.util.UUID;
 
 public class Notebook extends Prodotti{
 
-    public Notebook(TipoDispositivo setTipoDispositivo, String produttore, String modello, String descrizione, Double dimensioneDisplay,
-                    TipoMemoriaArchiviazione setTipoMemoria, Integer dimensioneArchiviazione, Double prezzoAcquisto, Double prezzoVendita, UUID id) {
-        super(setTipoDispositivo, produttore, modello, descrizione, dimensioneDisplay, setTipoMemoria, dimensioneArchiviazione, prezzoAcquisto, prezzoVendita, id);
+    public Notebook(TipoDispositivo tipoDispositivo, String produttore, String modello, String descrizione, Double dimensioneDisplay,
+                    TipoMemoriaArchiviazione tipoMemoria, Integer spazioArchiviazione, Double prezzoAcquisto, Double prezzoVendita, UUID id) {
+        super(tipoDispositivo, produttore, modello, descrizione, dimensioneDisplay, tipoMemoria, spazioArchiviazione, prezzoAcquisto, prezzoVendita, id);
     }
 }
+
