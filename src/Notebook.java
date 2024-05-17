6d7308afc9421a0f0afc2a86f71095a5eeaedd2a
Ilaria Faleschini <ilaria.faleschini@gmail.com>
2024-05-17 11:52:24 +0200

    bug fix Prodotto

--- src/Notebook.java
+++ src/Notebook.java
@@ -1,10 +1,55 @@
 import java.util.UUID;
 
-public class Notebook extends Prodotti{
+public class Notebook  {
 
-    public Notebook(TipoDispositivo tipoDispositivo, String produttore, String modello, String descrizione, Double dimensioneDisplay,
-                    TipoMemoriaArchiviazione tipoMemoria, SpazioDiArchiviazione spazioArchiviazione, Double prezzoAcquisto, Double prezzoVendita, UUID id) {
-        super(tipoDispositivo, produttore, modello, descrizione, dimensioneDisplay, tipoMemoria, spazioArchiviazione, prezzoAcquisto, prezzoVendita, id);
+    private TipoDispositivo tipoDispositivo;
+    private String produttore;
+    private String modello;
+    private Double dimensioneDisplay;
+    private TipoMemoriaArchiviazione tipoMemoria;
+    private SpazioDiArchiviazione dimensioneArchiviazione;
+    private Double prezzoAcquisto;
+    private Double prezzoVendita;
+    private UUID id;
+
+    public void setTipoDispositivo(TipoDispositivo tipoDispositivo) {
+        this.tipoDispositivo = tipoDispositivo;
+    }
+
+    public void setProduttore(String produttore) {
+        this.produttore = produttore;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public void setDimensioneDisplay(Double dimensioneDisplay) {
+        this.dimensioneDisplay = dimensioneDisplay;
+    }
+
+    public void setTipoMemoria(TipoMemoriaArchiviazione tipoMemoria) {
+        this.tipoMemoria = tipoMemoria;
+    }
+
+    public void setDimensioneArchiviazione(SpazioDiArchiviazione dimensioneArchiviazione) {
+        this.dimensioneArchiviazione = dimensioneArchiviazione;
+    }
+
+    public void setPrezzoAcquisto(Double prezzoAcquisto) {
+        this.prezzoAcquisto = prezzoAcquisto;
+    }
+
+    public void setPrezzoVendita(Double prezzoVendita) {
+        this.prezzoVendita = prezzoVendita;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Prodotto notebookBiuld(){
+        return new Prodotto(tipoDispositivo,produttore,modello,dimensioneDisplay,tipoMemoria,dimensioneArchiviazione,prezzoAcquisto,prezzoVendita,id);
     }
 }
 
--- src/Tablet.java
+++ src/Tablet.java
@@ -1,8 +1,54 @@
 import java.util.UUID;
 
-public class Tablet extends Prodotti  {
+public class Tablet {
 
-    public Tablet(TipoDispositivo tipoDispositivo, String produttore, String modello, String descrizione, Double dimensioneDisplay, TipoMemoriaArchiviazione tipoMemoria, SpazioDiArchiviazione dimensioneArchiviazione, Double prezzoAcquisto, Double prezzoVendita, UUID id) {
-        super(tipoDispositivo, produttore, modello, descrizione, dimensioneDisplay, tipoMemoria, dimensioneArchiviazione, prezzoAcquisto, prezzoVendita, id);
+    private TipoDispositivo tipoDispositivo;
+    private String produttore;
+    private String modello;
+    private Double dimensioneDisplay;
+    private TipoMemoriaArchiviazione tipoMemoria;
+    private SpazioDiArchiviazione dimensioneArchiviazione;
+    private Double prezzoAcquisto;
+    private Double prezzoVendita;
+    private UUID id;
+
+    public void setTipoDispositivo(TipoDispositivo tipoDispositivo) {
+        this.tipoDispositivo = tipoDispositivo;
+    }
+
+    public void setProduttore(String produttore) {
+        this.produttore = produttore;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public void setDimensioneDisplay(Double dimensioneDisplay) {
+        this.dimensioneDisplay = dimensioneDisplay;
+    }
+
+    public void setTipoMemoria(TipoMemoriaArchiviazione tipoMemoria) {
+        this.tipoMemoria = tipoMemoria;
+    }
+
+    public void setDimensioneArchiviazione(SpazioDiArchiviazione dimensioneArchiviazione) {
+        this.dimensioneArchiviazione = dimensioneArchiviazione;
+    }
+
+    public void setPrezzoAcquisto(Double prezzoAcquisto) {
+        this.prezzoAcquisto = prezzoAcquisto;
+    }
+
+    public void setPrezzoVendita(Double prezzoVendita) {
+        this.prezzoVendita = prezzoVendita;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Prodotto tabletBiuld(){
+        return new Prodotto(tipoDispositivo,produttore,modello,dimensioneDisplay,tipoMemoria,dimensioneArchiviazione,prezzoAcquisto,prezzoVendita,id);
     }
 }
